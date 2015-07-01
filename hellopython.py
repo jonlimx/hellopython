55fd97e22b2b113108b38cb05b99b4b5fa698528
Jonathan Lim <jonathanlim@yeah.net>
2015-07-01 14:15:14 +0800

    Initial commit

---
+++ hellopython.py
@@ -0,0 +1,34 @@
+__author__ = 'lin.zhongsheng'
+
+"""Return the sum result of the number array"""
+def sumArray(nums):
+    index = 0
+    result = 0
+    while index < len(nums):
+        result += nums[index]
+        index += 1
+
+    return result
+
+
+def avgArray(nums):
+    sum = sumArray(nums)
+
+    return float(sum) / len(nums)
+
+
+
+astr = ''
+
+nums = [1, 2.4, 5, 7, 6.5]
+
+while astr != 'X':
+    astr = raw_input('Please choose:')
+    if astr == '1':
+        print 'Sum of array: %f' % sumArray(nums)
+    elif astr == '2':
+        print 'Average of array: %f' % avgArray(nums)
+    elif astr == 'X':
+        print('Calculation finished!')
+    else:
+        print('Invalid input, please try again!')
