decdb3e1456f9407ee98dcc67c437c5cd32f4f28
Jonathan Lim <jonlimx@gmail.com>
2016-08-19 17:29:49 +0800

    1) Threading with deadlock avoidance. 2)Threading with thread pool.

---
+++ MultiThreading/ThreadingWithThreadPool.py
@@ -0,0 +1,23 @@
+from concurrent.futures import ThreadPoolExecutor
+import urllib.request
+
+
+def fetch_url(url):
+    u = urllib.request.urlopen(url)
+    data = u.read()
+    return data
+
+
+pool = ThreadPoolExecutor(10)
+
+# Submit work to thread pool
+a = pool.submit(fetch_url, 'http://www.python.org')
+b = pool.submit(fetch_url, 'http://www.pypy.org')
+
+if __name__ == '__main__':
+    # Get the results back
+    x = a.result()
+    y = b.result()
+
+print(x)
+print(y)
